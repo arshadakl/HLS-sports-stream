184b9b9f452a10995cfaa2f97eb772bbcd0c7e12
arshadakl <arshadakl@users.noreply.github.com>
2026-09-12 11:21:13 +0530

    chore: add site url and allowedHosts to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://REPLACE-WITH-YOUR-PROJECT.pages.dev',
   output: 'hybrid',
   adapter: node({ mode: 'standalone' }),
   integrations: [tailwind()],
+  vite: {
+    server: {
+      allowedHosts: true,
+    },
+  },
 });
